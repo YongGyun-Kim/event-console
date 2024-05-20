710f61d57838a936cda1811d86a9ccd893d14173
YongGyun-Kim <kyg7089@naver.com>
2024-05-21 00:21:35 +0900

    cdk init

---
+++ test/event-console.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EventConsole from '../lib/event-console-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/event-console-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EventConsole.EventConsoleStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
